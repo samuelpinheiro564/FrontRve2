a7d84b56b33beb002bfee49361fbfdbe1cbc102c
Victor Mattos <victormaria@aluno.senai.br>
2024-10-02 11:52:18 -0300

    Add Atestado page to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import Login from './pages/Login/page';
 // import CadastroUsuarios from './pages/CadastroUsuarios/page.js';
+import Atestado from './pages/Atestado/page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Atestado />
     {/* <CadastroUsuarios /> */}
     {/* <Login /> */}
   </React.StrictMode>
