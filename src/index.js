89128b18b94987b4d8e3c70ddd9ed18329195f19
Victor Mattos <victormaria@aluno.senai.br>
2024-10-02 16:31:15 -0300

    Refactorizar estilos de login y eliminar archivos y código innecesarios

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import Login from './pages/Login/page';
 // import CadastroUsuarios from './pages/CadastroUsuarios/page.js';
-import Atestado from './pages/Atestado/page';
-
+// import Atestado from './pages/Atestado/page';
+// import CategoriaDocente from './pages/CategoriaDocente/page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Atestado />
+    {/* <CategoriaDocente /> */}
+    {/* <Atestado /> */}
     {/* <CadastroUsuarios /> */}
     {/* <Login /> */}
   </React.StrictMode>
