c59a27fdd91b7c74cf7e05a7dbb91a53661aeea1
Your Name <you@example.com>
2024-10-02 17:16:30 -0300

    Refatorar importações e adicionar novas páginas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './pages/Login/page';
+import CadastroUsuarios from './pages/CadastroUsuarios/page';
+import Atestado from './pages/Atestado/page';
+import CategoriaDocente from './pages/CategoriaDocente/page';
 
-// import Login from './pages/Login/page';
-// import CadastroUsuarios from './pages/CadastroUsuarios/page.js';
-// import Atestado from './pages/Atestado/page';
-// import CategoriaDocente from './pages/CategoriaDocente/page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <CategoriaDocente /> */}
-    {/* <Atestado /> */}
-    {/* <CadastroUsuarios /> */}
-    {/* <Login /> */}
-
+    <CategoriaDocente />
+    <CadastroUsuarios />
+    <Login />
+    <Atestado />
   </React.StrictMode>
 );
