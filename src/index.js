7235f66300d94664e107a01f0912cc17dbb74727
Victor Mattos <victormaria@aluno.senai.br>
2024-10-02 11:24:31 -0300

    Remove commented out code and unused import statements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './pages/Login/page';
+// import Login from './pages/Login/page';
+// import CadastroUsuarios from './pages/CadastroUsuarios/page.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    {/* <CadastroUsuarios /> */}
+    {/* <Login /> */}
   </React.StrictMode>
 );
 
