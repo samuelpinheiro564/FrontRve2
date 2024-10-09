5d36697336a83dfa6decebeda4f1e9e3d91d8981
Pedro Santos <pedro.miguel@aluno.senai.>
2024-10-09 08:15:24 -0300

    feat: saida professor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import CadastroUsuarios from '../src/pages/CadastroUsuarios/CadastroUsuarios'
-
+/* import CadastroUsuarios from '../src/pages/CadastroUsuarios/CadastroUsuarios' */
+import SaidaProfessor from '../src/pages/saidaProfessor/SaidaProfessor'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <CadastroUsuarios />
+     <SaidaProfessor /> 
 
   </React.StrictMode>
 );
