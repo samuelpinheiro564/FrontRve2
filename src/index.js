238920171deedfa48aed189abf1b6ac75a168dab
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-10-21 07:46:52 -0300

    foi

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-<<<<<<< HEAD
-
 import Sidebar from '../src/pages/Sidebar/page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Sidebar/>
-=======
-// import CadastroUsuarios from '../src/pages/CadastroUsuarios/CadastroUsuarios' 
 
-import Atestado from './pages/Atestado/page';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-
-
-     <Atestado />
->>>>>>> 85784b5d242a195b3b85caf9f657a8272eac6e32
   </React.StrictMode>
 );
