362e50e7985a712e9cc2a9a2670ff0b3ede09fc5
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-10-21 08:57:47 -0300

    menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-import Atestado from './pages/Atestado/page';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import CategoriaAdmin from "./pages/CategoriaAdmin/page";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Atestado />
-    
-
+    <CategoriaAdmin />
   </React.StrictMode>
 );
