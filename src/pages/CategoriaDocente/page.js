07a71910124d3336595710f73b5af1f3dfc5e38b
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-10-21 08:41:14 -0300

    page

--- src/pages/CategoriaDocente/page.js
+++ src/pages/CategoriaDocente/page.js
@@ -1,28 +1,32 @@
 import React from 'react';  
 import { FaFileAlt, FaDoorOpen, FaUserGraduate } from 'react-icons/fa';  
 import '../CategoriaDocente/styles.modules.css';  
+import Sidebar from '../Sidebar/page';
 
 function App() {  
-  return (  
-    <div className="app">  
-      <header className="header">  
-        <h1>SENAI</h1>
-      </header>  
-      <div className="container">  
-        <div className="card">  
-          <FaFileAlt className="icon" />  
-          <h2>RVE</h2>  
-        </div>  
-        <div className="card">  
-          <FaDoorOpen className="icon" />  
-          <h2>Saída</h2>  
-        </div>  
-        <div className="card">  
-          <FaUserGraduate className="icon" />  
-          <h2>Alunos</h2>  
-        </div>  
-      </div>  
-    </div>  
+  return (
+    <div>
+      <Sidebar />
+      <div className="app">
+        <header className="header">
+          <h1>SENAI</h1>
+        </header>
+        <div className="container">
+          <div className="card">
+            <FaFileAlt className="icon" />
+            <h2>RVE</h2>
+          </div>
+          <div className="card">
+            <FaDoorOpen className="icon" />
+            <h2>Saída</h2>
+          </div>
+          <div className="card">
+            <FaUserGraduate className="icon" />
+            <h2>Alunos</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   );  
 }  
 
