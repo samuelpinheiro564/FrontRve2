ad90123eef9f4060db2358f166279fad6cb63c58
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-10-21 07:45:08 -0300

    trababalhos do picirili

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import CadastroUsuarios from '../src/pages/CadastroUsuarios/CadastroUsuarios' 
-import UserList from '../src/components/UserList';
+
+import Sidebar from '../src/pages/Sidebar/page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
-     {/* <CadastroUsuarios />  */}
-     <UserList />
+    <Sidebar/>
   </React.StrictMode>
 );
