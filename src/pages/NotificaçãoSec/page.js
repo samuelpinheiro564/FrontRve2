3225b31bec365c8fd577a9ab361200ff986e83fe
Victor Mattos <victormaria@aluno.senai.br>
2024-10-21 11:19:44 -0300

    Refatorar código da página de Notificação Sec e adicionar estilo para o campo RA

---
+++ src/pages/NotificaçãoSec/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotificaçãoSec = () => {
+    return (
+        <div>
+            <h1>Notificação Sec</h1>
+            <p>This is the Notificação Sec page.</p>
+        </div>
+    );
+};
+
+export default NotificaçãoSec;
