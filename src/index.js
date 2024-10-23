5368e2c2db611a638700a3d6c94d2bac4ee9c223
Victor Hugo Mattos Maria <victormaria@aluno.senai.br>
2024-10-23 11:26:01 -0300

    Refatorar dependências e estilos para adicionar suporte ao React Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Rve from '../src/pages/Rve/Rve'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Saida from "../src/Routes/Saida/page";
+import Rve from "../src/Routes/Rve/Rve";
+import Login from '../src/Routes/Login/Login';
+import SaidaProfessor from "../src/Routes/saidaProfessor/page";
+import CategoriaAdmin from './Routes/CategoriaAdmin/page';
+// import CadastroUsuarios from '../src/Routes/CadastroUsuarios/CadastroUsuarios';
+import AlunoAdm from "../src/Routes/alunosAdm/alunosAdm";
+import CategoriDocente from "../src/Routes/CategoriaDocente/page";
+import ErrorPage from './Routes/ErrorPage';
 
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "Saida",
+        element: <Saida />,
+      },
+      {
+        path: "Rve",
+        element: <Rve />,
+      },
+      {
+        path: "SaidaProfessor",
+        element: <SaidaProfessor />,
+      },
+      // {
+      //   path: "CadastroUsuarios",
+      //   element: <CadastroUsuarios />,
+      // },
+      {
+        path: "CategoriDocente",
+        element: <CategoriDocente />,
+      },
+      {
+        path: "CategoriaAdmin",
+        element: <CategoriaAdmin />,
+      },
+      {
+        path: "AlunoAdm",
+        element: <AlunoAdm />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<Rve/>
-
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+export default App;
