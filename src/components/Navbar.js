2c603b60741837f8a2d3328e58c149c8823e6151
Victor Hugo Mattos Maria <victormaria@aluno.senai.br>
2024-10-28 17:11:53 -0300

    Refatorar estilos do menu de navegação

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,48 @@
-import React from 'react';  
+import React, { useState } from 'react';  
 import { NavLink } from 'react-router-dom';  
 import './Navbar.css'; // opcional para estilos  
 
 const NavBar = () => {  
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (  
-    <nav>  
-      <ul>  
-        <li>  
-          <NavLink to="/SuasRve" activeClassName="active">Suas RVE</NavLink>  
-        </li>  
-        <li>  
-          <NavLink to="/Saida" activeClassName="active">Saída</NavLink>  
-        </li>  
-        <li>  
-          <NavLink to="/Rve" activeClassName="active">RVE</NavLink>  
-        </li>  
-        <li>  
-          <NavLink to="/CategoriaDocente" activeClassName="active">Categoria Docente</NavLink>  
-        </li>  
-        <li>  
-          <NavLink to="/CategoriaAdmin" activeClassName="active">Categoria Admin</NavLink>  
-        </li> 
-        <li>  
-          <NavLink to="/CadastroUsuarios" activeClassName="active">Cadastro USer</NavLink>  
-        </li>   
-        <li>  
-          <NavLink to="/NotificacaoSec" activeClassName="active">Notificação Sec</NavLink>  
-        </li>  
-      </ul>  
-    </nav>  
+    <div>
+      <button onClick={toggleMenu} className="menu-icon">
+        &#9776; {/* Ícone de menu (hambúrguer) */}
+      </button>
+      {menuVisible && (
+        <nav>  
+          <ul>  
+            <li>  
+              <NavLink to="/SuasRve" activeClassName="active">Suas RVE</NavLink>  
+            </li>  
+            <li>  
+              <NavLink to="/Saida" activeClassName="active">Saída</NavLink>  
+            </li>  
+            <li>  
+              <NavLink to="/Rve" activeClassName="active">RVE</NavLink>  
+            </li>  
+            <li>  
+              <NavLink to="/CategoriaDocente" activeClassName="active">Categoria Docente</NavLink>  
+            </li>  
+            <li>  
+              <NavLink to="/CategoriaAdmin" activeClassName="active">Categoria Admin</NavLink>  
+            </li> 
+            <li>  
+              <NavLink to="/CadastroUsuarios" activeClassName="active">Cadastro USer</NavLink>  
+            </li>   
+            <li>  
+              <NavLink to="/NotificacaoSec" activeClassName="active">Notificação Sec</NavLink>  
+            </li>  
+          </ul>  
+        </nav>
+      )}
+    </div>
   );  
 };  
+
 export default NavBar;
