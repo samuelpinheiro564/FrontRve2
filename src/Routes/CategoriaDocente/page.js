b7d906c4a1843334c07623198ed572a298572ddb
Victor Mattos <victormaria@aluno.senai.br>
2024-10-28 11:17:56 -0300

    Refactor styles.modules.css and docente.module.css

--- src/Routes/CategoriaDocente/page.js
+++ src/Routes/CategoriaDocente/page.js
@@ -1,27 +1,25 @@
 import React from 'react';  
 import { FaFileAlt, FaDoorOpen, FaUserGraduate } from 'react-icons/fa';  
-import './styles.modules.css';  
+import styles from "../CategoriaDocente/docente.module.css";
 
 function App() {  
   return (
-    <div>
-      <div className="app">
-        <header className="header">
-          <h1>SENAI</h1>
-        </header>
-        <div className="container">
-          <div className="card">
-            <FaFileAlt className="icon" />
-            <h2>RVE</h2>
-          </div>
-          <div className="card">
-            <FaDoorOpen className="icon" />
-            <h2>Saída</h2>
-          </div>
-          <div className="card">
-            <FaUserGraduate className="icon" />
-            <h2>Alunos</h2>
-          </div>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <h1>SENAI</h1>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <FaFileAlt className={styles.icon} />
+          <h2>RVE</h2>
+        </div>
+        <div className={styles.card}>
+          <FaDoorOpen className={styles.icon} />
+          <h2>Saída</h2>
+        </div>
+        <div className={styles.card}>
+          <FaUserGraduate className={styles.icon} />
+          <h2>Alunos</h2>
         </div>
       </div>
     </div>
