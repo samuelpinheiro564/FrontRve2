2c659b05635d0c89e8fbe4a7697ab78ac99350e4
Victor Mattos <victormaria@aluno.senai.br>
2024-10-28 11:17:45 -0300

    Refactor CadastroUsuarios component and add styles to form elements

--- src/Routes/CategoriaAdmin/page.js
+++ src/Routes/CategoriaAdmin/page.js
@@ -1,28 +1,26 @@
 import React from "react";
-import "./styles.modules.css"; 
+import styles from "../CategoriaAdmin/admin.module.css"; 
 
 const CategoriaAdmin = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="banner">
-          <h1>SENAI</h1>
-        </div>
-        <h2>Categorias</h2>
-        <div className="cards-background">
-          <div className="grid">
-            <div className="card active">
-              <i className="icon">📝</i>
-              <p>RVE</p>
-            </div>
-            <div className="card">
-              <i className="icon">📤</i>
-              <p>Saída</p>
-            </div>
-            <div className="card1">
-              <i className="icon">👥</i>
-              <p>Usuários</p>
-            </div>
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <h1>SENAI</h1>
+      </div>
+      <h2>Categorias</h2>
+      <div className={styles.cardsBackground}>
+        <div className={styles.grid}>
+          <div className={`${styles.card} ${styles.active}`}>
+            <i className={styles.icon}>📝</i>
+            <p>RVE</p>
+          </div>
+          <div className={styles.card}>
+            <i className={styles.icon}>📤</i>
+            <p>Saída</p>
+          </div>
+          <div className={styles.card1}>
+            <i className={styles.icon}>👥</i>
+            <p>Usuários</p>
           </div>
         </div>
       </div>
