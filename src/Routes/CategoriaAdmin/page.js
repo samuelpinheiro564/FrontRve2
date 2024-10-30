62d90ab1811f70abee662acfe486ba1969d38a5f
Victor Mattos <victormaria@aluno.senai.br>
2024-10-30 14:31:16 -0300

    Atualizar estilos e otimizar navegação na página de administração de categorias

--- src/Routes/CategoriaAdmin/page.js
+++ src/Routes/CategoriaAdmin/page.js
@@ -1,29 +1,35 @@
 import React from "react";
 import styles from "../CategoriaAdmin/admin.module.css"; 
-
+import { NavLink } from "react-router-dom";
 const CategoriaAdmin = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.banner}>
-        <h1>SENAI</h1>
-      </div>
-      <h2>Categorias</h2>
-      <div className={styles.cardsBackground}>
-        <div className={styles.grid}>
+    <div className={styles.container}>  
+      <div className={styles.banner}>  
+        <h1>SENAI</h1>  
+      </div>  
+      <h2>Categorias</h2>  
+      <div className={styles.cardsBackground}>  
+        <div className={styles.grid}>  
           <div className={`${styles.card} ${styles.active}`}>
-            <i className={styles.icon}>📝</i>
-            <p>RVE</p>
-          </div>
-          <div className={styles.card}>
-            <i className={styles.icon}>📤</i>
-            <p>Saída</p>
-          </div>
-          <div className={styles.card1}>
-            <i className={styles.icon}>👥</i>
-            <p>Usuários</p>
-          </div>
-        </div>
-      </div>
+            <NavLink to="/Rve" activeClassName={styles.active}>  
+          <i className={styles.icon}>📝</i>  
+            <p>RVE</p>  
+           </NavLink>
+          </div>  
+          <NavLink to="/Saida" activeClassName={styles.active}>
+          <div className={styles.card}>  
+          <i className={styles.icon}>📤</i> 
+            <p>Saída</p>  
+          </div>  
+          </NavLink>
+          <NavLink to="/CadastroUsuarios" activeClassName={styles.active}>
+          <div className={styles.card1}>  
+          <i className={styles.icon}>👥</i>
+            <p>Usuários</p>  
+          </div>  
+          </NavLink>
+        </div>  
+      </div>  
     </div>
   );
 };
