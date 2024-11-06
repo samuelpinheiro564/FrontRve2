e9b0f1a0a6014df77ac1c7dfe69ae6104185b489
DanielSP019 <daniel.santos3@aluno.senai.br>
2024-11-06 14:34:25 -0300

    Refatora nome de classe e variáveis no módulo DadosRve.js

--- src/Data/DadosRve.js
+++ src/Data/DadosRve.js
@@ -1,49 +1,43 @@
-class dadosRVE {  
+class dadosRve {  
     constructor() {  
-        // Inicializa a lista de RVE a partir do sessionStorage, se disponível  
+        // Inicializa a lista de rves a partir do sessionStorage, se disponível  
         this.rves = this.getRvesFromStorage();  
     }  
-
-    // Adiciona um RVE ao array e ao sessionStorage  
+  
+    // Adiciona um rve ao array e ao sessionStorage  
     addRve(rve) {  
         this.rves.push(rve);  
         this.saveRvesToStorage();  
     }  
-
-    // Adiciona assinaturas a um RVE e salva no sessionStorage  
-    addAssinaturas(assinaturas) {  
-        this.rves.push(assinaturas);  
-        this.saveRvesToStorage();  
-    }  
-
-    // Obtém todos os RVE  
+  
+    // Obtém todos os rves  
     getRves() {  
         return this.rves;  
     }  
-
-    // Encontra um RVE pelo ID  
-    findRve(rveId) {  
-        return this.rves.find((rve) => rve.rveId === rveId);  
+  
+    // Encontra um rve pelo nome de usuário  
+    findRve(username) {  
+        return this.rves.find((rve) => rve.username === username);  
     }  
-
-    // Limpa todos os dados dos RVE  
+  
+    // Limpa todos os dados dos rves  
     clearRves() {  
         this.rves = [];  
         sessionStorage.removeItem('rves'); // Limpa os dados do sessionStorage  
     }  
-
-    // Salva os RVE no sessionStorage  
+  
+    // Salva os rves no sessionStorage  
     saveRvesToStorage() {  
         sessionStorage.setItem('rves', JSON.stringify(this.rves));  
     }  
-
-    // Obtém os RVE do sessionStorage  
+  
+    // Obtém os rves do sessionStorage  
     getRvesFromStorage() {  
         const storedRves = sessionStorage.getItem('rves');  
         return storedRves ? JSON.parse(storedRves) : [];  
     }  
 }  
-
+  
 // Instância única da classe para ser usada em toda a aplicação  
-const rveData = new dadosRVE();  
+const rveData = new dadosRve();  
 export default rveData;
