9cf2c3444c1a6228c6282f1867f98be97df7a97c
Samuel Pinheiro <samuel.pinheiro2@aluno.senai.br>
2024-11-11 14:12:07 -0300

    refatora função fetchAllMsg para ser chamada corretamente no efeito colateral

--- src/Routes/Rve/Rve.jsx
+++ src/Routes/Rve/Rve.jsx
@@ -155,20 +155,13 @@ const Rve = () => {
   useEffect(() => {
     if (chatAtivo) return;
 
-    /**
-     * Fetches all messages by calling the AllCamposTextoRve function and updates the state with the retrieved messages.
-     * 
-     * @async
-     * @function fetchAllMsg
-     * @returns {Promise<void>} A promise that resolves when the messages have been fetched and the state has been updated.
-     */
-  //  const fetchAllMsg = async () => {
-  //    const allMessages = await AllCamposTextoRve();
-  //    setMsgs(allMessages);
- //   };
+    const fetchAllMsg = async () => {
+      const allMessages = await AllCamposTextoRve();
+      setMsgs(allMessages);
+    };
 
- //   fetchAllMsg();
-//  }, [chatAtivo]);
+    fetchAllMsg();
+  }, [chatAtivo]);
 
   return (
     <div className={styles.container}>
@@ -391,7 +384,7 @@ const Rve = () => {
         </>
       )}
     </div>
-}));
-  };
+  );
+};
 
 export default Rve;
