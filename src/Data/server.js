2ccdc35974915f5c8ecbfe8caad27716e47df0e3
samuelpinheiro564 <samuel.pinheiro2@aluno.senai.br>
2024-11-28 07:52:23 -0300

    fix: atualiza URL do servidor para o novo endereço IP

--- src/Data/server.js
+++ src/Data/server.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://10.88.200.162:4040';
+const URL = 'http://192.168.0.88:4040';
 const LoginUser = async (nif,senha) => {
     try {
         const requestURL = `${URL}/usuarios/${nif}/${senha}`;
