71a2d59547020a639c72584d74f6d651137a55e3
samuelpinheiro564 <samuel.pinheiro2@aluno.senai.br>
2024-12-02 00:20:43 -0300

    feat: ajustar estilos de componentes e melhorar responsividade nas páginas de CadastroUsuarios, CategoriaAdmin e HistóricoSaida

--- src/Routes/CategoriaAdmin/page.jsx
+++ src/Routes/CategoriaAdmin/page.jsx
@@ -1,38 +1,44 @@
 import React from "react";
 import styles from "../CategoriaAdmin/admin.module.css";
 import { NavLink } from "react-router-dom";
-
 const CategoriaAdmin = () => {
   return (
     <div className={styles.container}>
+      
       <div className={styles.banner}>
+        
         <h1>SENAI</h1>
       </div>
-      
       <div className={styles.cardsBackground}>
+        
         <div className={styles.grid}>
+          
           <NavLink to="/Rve" activeClassName={styles.active}>
+            
             <div className={`${styles.card} ${styles.active}`}>
-              <i className={styles.icon}>📝</i>
-              <p>RVE</p>
+              
+              <i className={styles.icon}>📝</i> <p>RVE</p>
             </div>
           </NavLink>
           <NavLink to="/Saida" activeClassName={styles.active}>
+            
             <div className={styles.card}>
-              <i className={styles.icon}>📤</i>
-              <p>Saída</p>
+              
+              <i className={styles.icon}>📤</i> <p>Saída</p>
             </div>
           </NavLink>
           <NavLink to="/CadastroUsuarios" activeClassName={styles.active}>
+            
             <div className={styles.card1}>
-              <i className={styles.icon}>👥</i>
-              <p>Usuários</p>
+              
+              <i className={styles.icon}>👥</i> <p>Usuários</p>
             </div>
           </NavLink>
           <NavLink to="/HistoricoSaida" activeClassName={styles.active}>
+            
             <div className={styles.card2}>
-              <i className={styles.icon}>📃</i>
-              <p>Historico Saida</p>
+              
+              <i className={styles.icon}>📃</i> <p>Historico Saida</p>
             </div>
           </NavLink>
         </div>
@@ -40,5 +46,4 @@ const CategoriaAdmin = () => {
     </div>
   );
 };
-
 export default CategoriaAdmin;
