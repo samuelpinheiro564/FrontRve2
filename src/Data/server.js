4a210d5e68053590811c74f84eeb70b0d06340a7
Your Name <you@example.com>
2024-12-02 07:36:19 -0300

    feat: atualizar URL do servidor para nova configuração de rede

--- src/Data/server.js
+++ src/Data/server.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://192.168.0.88:4040';
+const URL = 'http://10.88.200.162:4040';
 const LoginUser = async (nif, senha) => {
     try {
         // Construindo a URL com os parâmetros `nif` e `senha`
